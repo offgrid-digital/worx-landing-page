51bd1129e3ccc606352b551910f3a13f3dc707e9
unknown <aquib_j@yahoo.com>
2023-05-23 12:07:37 +0530

    Carousel Added in the Worx Property Section

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 $('.worx_testimomials_mbl_carousel > .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dot: true,
+    // animateOut: 'slideOutDown',
+    // animateIn: 'flipInX',
+    // smartSpeed: 450,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 1
+        },
+        767: {
+            items: 1.1
+        }
+    }
+});
+
+
+$('.worx_property_mbl > .owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav: false,
