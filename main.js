364b3228a0f10933beba14f1828becacacb47290
unknown <aquib_j@yahoo.com>
2023-05-23 12:13:19 +0530

    Font size updated for mobile section

--- main.js
+++ main.js
@@ -25,26 +25,52 @@ $('.worx_testimomials_mbl_carousel > .owl-carousel').owlCarousel({
 
 
 $('.worx_property_mbl > .owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
+    loop: true,
+    margin: 10,
     nav: false,
     dot: true,
     // animateOut: 'slideOutDown',
     // animateIn: 'flipInX',
     // smartSpeed: 450,
-    autoplay:true,
-    autoplayTimeout:2000,
+    autoplay: true,
+    autoplayTimeout: 2000,
     
-    responsive:{
-        0:{
-            items:1,
+    responsive: {
+        0: {
+            items: 1,
             
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        767:{
-            items:1.1
+        767: {
+            items: 1.1
         }
     }
-})
+});
+
+$('.worx_trusted_mbl > .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dot: true,
+    // animateOut: 'slideOutDown',
+    // animateIn: 'flipInX',
+    // smartSpeed: 450,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 1
+        },
+        767: {
+            items: 1.1
+        }
+    }
+});
+
