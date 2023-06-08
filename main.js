7c7efebc64d1f05bd74d4573f8b4dc95ce0c82b4
unknown <aquib_j@yahoo.com>
2023-06-08 23:08:56 +0530

    Dispenser Modal added

--- main.js
+++ main.js
@@ -118,7 +118,61 @@ $(document).ready(function(){
         // $("#mediumProduct").trigger('click');
         $(".tab_content").hide();
         $(".tab_content:nth-child(3)").show();
+
     }, 3500);
+
+
+    setTimeout(function () {
+
+
+        $(function() {
+
+            //find iframe
+            let iframe = $('#product-component-1685341297708 > iframe');
+            
+            //find button inside iframe
+            let button = iframe.contents().find('.shopify-buy__btn-wrapper > button');
+            
+            //trigger button click
+            button.click(function () {                
+                setTimeout(function () {
+                    console.log("The paragraph was clicked.");  
+                    $("#exampleModal").modal('show')
+                }, 1500);
+            });
+            
+        });
+
+
+        $(function() {
+
+            //find iframe
+            let iframe = $('#product-component-1685415660713 > iframe');
+            
+            //find button inside iframe
+            let button = iframe.contents().find('.shopify-buy__btn-wrapper > button');
+            
+            //trigger button click
+            button.click(function () {             
+                setTimeout(function () {
+                    console.log("The paragraph was clicked.");
+                    $("#faq").trigger('click')
+                }, 1000);
+                setTimeout(function () {
+                    console.log("The paragraph was clicked.");  
+                    $("#exampleModal").modal('show')
+                }, 1800);
+            });
+            console.log(button);
+        });
+
+
+        
+
+    }, 2500);
+
+
+    
 });
 
 
@@ -149,3 +203,211 @@ $(document).ready(function(){
 	   of last tab */
 	// $('ul.tabs li').last().addClass("tab_last");
 	
+
+/*<![CDATA[*/
+
+$("#product-component-1685341297708 > iframe > .shopify-buy__product > .shopify-buy__btn-wrapper").hide()
+
+function openModal() {
+    console.log('Hello');
+}
+
+
+var myModalEl = document.getElementById('exampleModal')
+myModalEl.addEventListener('shown.bs.modal', function (event) {
+    
+    
+
+    (function () {
+
+        // $("#loader").hide()
+        
+
+        var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+        if (window.ShopifyBuy) {
+            if (window.ShopifyBuy.UI) {
+                ShopifyBuyInit();
+            } else {
+                loadScript();
+            }
+        } else {
+            loadScript();
+        }
+        function loadScript() {
+            var script = document.createElement('script');
+            script.async = true;
+            script.src = scriptURL;
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+            script.onload = ShopifyBuyInit;
+        }
+        function ShopifyBuyInit() {
+            var client = ShopifyBuy.buildClient({
+                domain: 'worx-environmental-australia.myshopify.com',
+                storefrontAccessToken: 'd6e87929c08f447624bf912ac20cc17d',
+            });
+            ShopifyBuy.UI.onReady(client).then(function (ui) {
+                ui.createComponent('product', {
+                    id: '8208048324864',
+                    node: document.getElementById('product-component-1686234193437'),
+                    moneyFormat: '%24%7B%7Bamount%7D%7D',
+                    options: {
+                        "product": {
+                            "styles": {
+                                "product": {
+                                    "@media (min-width: 601px)": {
+                                        "max-width": "calc(25% - 20px)",
+                                        "margin-left": "20px",
+                                        "margin-bottom": "50px"
+                                    }
+                                },
+                                "button": {
+                                    "font-family": "Montserrat, sans-serif",
+                                    "font-weight": "bold",
+                                    "font-size": "18px",
+                                    "padding-top": "17px",
+                                    "padding-bottom": "17px",
+                                    ":hover": {
+                                        "background-color": "#182e83"
+                                    },
+                                    "background-color": "#0e1b4d",
+                                    ":focus": {
+                                        "background-color": "#182e83"
+                                    },
+                                    "border-radius": "35px",
+                                    "padding-left": "74px",
+                                    "padding-right": "74px"
+                                },
+                                "quantityInput": {
+                                    "font-size": "18px",
+                                    "padding-top": "17px",
+                                    "padding-bottom": "17px"
+                                }
+                            },
+                            "text": {
+                                "button": "Add to cart"
+                            },
+                            "googleFonts": [
+                                "Montserrat"
+                            ]
+                        },
+                        "productSet": {
+                            "styles": {
+                                "products": {
+                                    "@media (min-width: 601px)": {
+                                        "margin-left": "-20px"
+                                    }
+                                }
+                            }
+                        },
+                        "modalProduct": {
+                            "contents": {
+                                "img": false,
+                                "imgWithCarousel": true,
+                                "button": false,
+                                "buttonWithQuantity": true
+                            },
+                            "styles": {
+                                "product": {
+                                    "@media (min-width: 601px)": {
+                                        "max-width": "100%",
+                                        "margin-left": "0px",
+                                        "margin-bottom": "0px"
+                                    }
+                                },
+                                "button": {
+                                    "font-family": "Montserrat, sans-serif",
+                                    "font-weight": "bold",
+                                    "font-size": "18px",
+                                    "padding-top": "17px",
+                                    "padding-bottom": "17px",
+                                    ":hover": {
+                                        "background-color": "#182e83"
+                                    },
+                                    "background-color": "#0e1b4d",
+                                    ":focus": {
+                                        "background-color": "#182e83"
+                                    },
+                                    "border-radius": "35px",
+                                    "padding-left": "74px",
+                                    "padding-right": "74px"
+                                },
+                                "quantityInput": {
+                                    "font-size": "18px",
+                                    "padding-top": "17px",
+                                    "padding-bottom": "17px"
+                                }
+                            },
+                            "googleFonts": [
+                                "Montserrat"
+                            ],
+                            "text": {
+                                "button": "Add to cart"
+                            }
+                        },
+                        "option": {},
+                        "cart": {
+                            "styles": {
+                                "button": {
+                                    "font-family": "Montserrat, sans-serif",
+                                    "font-weight": "bold",
+                                    "font-size": "18px",
+                                    "padding-top": "17px",
+                                    "padding-bottom": "17px",
+                                    ":hover": {
+                                        "background-color": "#182e83"
+                                    },
+                                    "background-color": "#0e1b4d",
+                                    ":focus": {
+                                        "background-color": "#182e83"
+                                    },
+                                    "border-radius": "35px"
+                                }
+                            },
+                            "text": {
+                                "total": "Subtotal",
+                                "button": "Checkout"
+                            },
+                            "popup": false,
+                            "googleFonts": [
+                                "Montserrat"
+                            ]
+                        },
+                        "toggle": {
+                            "styles": {
+                                "toggle": {
+                                    "font-family": "Montserrat, sans-serif",
+                                    "font-weight": "bold",
+                                    "background-color": "#0e1b4d",
+                                    ":hover": {
+                                        "background-color": "#182e83"
+                                    },
+                                    ":focus": {
+                                        "background-color": "#182e83"
+                                    }
+                                },
+                                "count": {
+                                    "font-size": "18px"
+                                }
+                            },
+                            "googleFonts": [
+                                "Montserrat"
+                            ]
+                        }
+                    },
+                });
+            });
+        }
+    })()
+
+    
+    var x = document.getElementById("loader");
+    x.style.display = 'none'
+    console.log(x);
+    
+
+})
+
+
+
+    
+/*]]>*/
